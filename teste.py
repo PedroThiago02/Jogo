06acb0a7d98d9fc38d9682f6ac26ad07c66ed082
Pedro Thiago <144968307+PedroThiago02@users.noreply.github.com>
2025-04-07 08:48:19 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-nome=str(input('Digite seu nome: '))
-print('Seja bem vindo {} :)'.format(nome))
-print('{}, aqui você vai descobrir se seus números são ímpares ou pares! Vamos nessa?'.format(nome))
-numero=int(input('Qual número você quer descobrir se é ímpar ou par? '))
-if (numero%2==0):
-    print('{} é um número par'.format(numero))
-else:
-    print('{} é um número ímpar'.format(numero))
